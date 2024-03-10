9a72d177d5bf7cd8c718216930be3082e4139569
imeshaDewmin <dewmin2001@gmail.com>
2024-03-10 18:16:41 +0530

    Complete signup page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,17 @@
 import { Button, Navbar, TextInput } from 'flowbite-react';
-import { Link,useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
 
 export default function Header() {
-    const path= useLocation().pathname;
+    const path = useLocation().pathname;
     return (
         <Navbar className='border-b-2'>
             <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg
-            text-white'>Dewmin's</span>
+                <span className='px-2 py-1 bg-gradient-to-r from-green-600 via-black to-blue-900 rounded-lg text-white'>Dewmin's</span>
                 Blog
             </Link>
+
             <form>
                 <TextInput
                     type='text'
@@ -37,19 +37,19 @@ export default function Header() {
 
             </div>
             <Navbar.Collapse>
-                <Navbar.Link active={path==="/"} as={'div'} >
+                <Navbar.Link active={path === "/"} as={'div'} >
                     <Link to='/'>
                         Home
                     </Link>
                 </Navbar.Link>
 
-                <Navbar.Link active={path==="/about"} as={'div'} >
+                <Navbar.Link active={path === "/about"} as={'div'} >
                     <Link to='/about'>
                         About
                     </Link>
                 </Navbar.Link>
 
-                <Navbar.Link active={path==="/projects"} as={'div'}>
+                <Navbar.Link active={path === "/projects"} as={'div'}>
                     <Link to='/projects'>
                         Projects
                     </Link>
