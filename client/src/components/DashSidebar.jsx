6bc93cee834b3b503a11a2f68a9937d62903f8bc
imeshaDewmin <dewmin2001@gmail.com>
2024-03-17 11:31:28 +0530

    Add show users to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from 'flowbite-react';
-import { HiArrowSmRight, HiDocumentText, HiUser } from 'react-icons/hi';
+import { HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
@@ -39,7 +39,7 @@ export default function DashSidebar() {
             <Sidebar.Items>
                 <Sidebar.ItemGroup className='flex flex-col gap-1'>
                     <Link to='/dashboard?tab=profile'>
-                        <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin':'User'} labelColor='dark' as='div'>
+                        <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' as='div'>
                             Profile
                         </Sidebar.Item>
                     </Link>
@@ -54,6 +54,18 @@ export default function DashSidebar() {
                             </Sidebar.Item>
                         </Link>
                     )}
+                    {currentUser.isAdmin && (
+                        <Link to='/dashboard?tab=users'>
+                            <Sidebar.Item
+                                active={tab === 'users'}
+                                icon={HiOutlineUserGroup}
+                                as='div'
+                            >
+                                Users
+                            </Sidebar.Item>
+                        </Link>
+                    )}
+
 
                     <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignOut}>
                         Sign Out
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,8 @@ import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
+
 
 export default function Dashboard() {
   const loaction = useLocation()
@@ -26,6 +28,10 @@ export default function Dashboard() {
 
       {/*posts...*/}
       {tab === 'posts' && <DashPosts />}
+
+      {/*users*/}
+      {tab === 'users' && <DashUsers />}
+
     </div>
   )
 }
